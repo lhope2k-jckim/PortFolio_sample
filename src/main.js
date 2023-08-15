aaa520fdbdae59c2e398cd7b70cab0123bbaca1c
김종채 <Lhope2k@mindwareworks.com>
2023-08-15 23:57:25 +0900

    JS - ArrowUp Opacity

--- src/main.js
+++ src/main.js
@@ -15,3 +15,13 @@ const homeHeight = home.offsetHeight;
 document.addEventListener('scroll', () => {
     home.style.opacity = 1 - window.scrollY / homeHeight;
 });
+
+// 아래로 스크롤시, Arrow Up 버튼을 나타나게 처리
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if (window.scrollY > homeHeight / 2.0) {
+        arrowUp.style.opacity = 1;
+    } else {
+        arrowUp.style.opacity = 0;
+    }
+});
